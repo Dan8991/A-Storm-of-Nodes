ea95e546006d4a067fd316f66c790accea615d00
Dan8991 <daniseas98@gmail.com>
2019-12-04 11:05:18 +0100

    [Add] non charachter names and basic punctuation parsing

--- got_parser.py
+++ got_parser.py
@@ -1,6 +1,9 @@
 import PyPDF2 as pdf
 import enchant
+import sys
 from functools import reduce
+import re
+
 class Chapter(object):
 
     def __init__(self, title, words):
@@ -15,14 +18,14 @@ class Chapter(object):
     def __str__(self):
         return "chapter name = {}\nchapter length = {}".format(self.title, len(self.words))
 
-    def get_non_english_words(self):
+    def get_non_english_words(self, non_names = None):
         
         dictionary = enchant.Dict("en_UK")
         words = self.words.split()
         non_english_words = []
         last_non_eng = False
         for word in words:
-            if not dictionary.check(word) and word[0].isupper():
+            if not dictionary.check(word) and word[0].isupper() and (word not in non_names):
                 if last_non_eng:
                     non_english_words[-1] += " " + word
                 else:
@@ -42,7 +45,7 @@ def get_chapters_from_text(text):
     chapters = []
 
     for line in text[1:]:
-        if line.isupper():
+        if line.isupper() and (len(line.split()) == 1):
             chapters.append(Chapter(title, words))
             title = line
             words = ""
@@ -51,10 +54,42 @@ def get_chapters_from_text(text):
 
     return chapters
 
+def parse_punctuation(s):
+
+    #changing all puntuation to a comma and separating it from words
+    parsed = re.sub(r"[\.,!?”“…]", " , ", s)
+    
+    #removing multiple spaces
+    parsed = " ".join(parsed.split()) + "\n"
+
+    #removing saxons genitive and other similar things because it is annoying for parsing
+    parsed = re.sub(r"(’s|’n|’d)", "", parsed)
+
+    parsed = re.sub(r"-\n", "", parsed)
+
+    return parsed
+
+def get_non_names(file_name):
+    return set([line.replace("\n", "") for line in open(file_name, 'r')])
+
+if (len(sys.argv) > 1) and (sys.argv[1] == "parse"):
+    
+    with open("storm_of_swords.txt", "r") as f:
+        lines = [parse_punctuation(line) for line in f]
+        
+    with open("storm_of_swords.txt", "w") as f:
+        f.writelines(lines)
+
 
 pdf_file = open("storm_of_swords.txt", 'r')
 
 chapters = get_chapters_from_text([line for line in pdf_file])
 
-unique = list(reduce(lambda x, y: x.union(y), [chapter.get_non_english_words() for chapter in chapters]))
-print(len(unique))
+non_names = get_non_names("non_character_names.txt")
+
+if sys.argv[-1] == -1:
+    unique = list(reduce(lambda x, y: x.union(y), [chapter.get_non_english_words(non_names = non_names) for chapter in chapters]))
+else: 
+    unique = chapters[int(sys.argv[-1])].get_non_english_words(non_names = non_names)
+print("Unique characters found: {}", len(unique))
+print(unique)
